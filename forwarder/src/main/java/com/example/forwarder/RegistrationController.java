91a607531522e387cf2e2707947ce6d8121931f5
Jonas <jonasx.dufek@gmail.com>
2025-11-13 16:02:58 +0100

    dev checkpoint

--- forwarder/src/main/java/com/example/forwarder/RegistrationController.java
+++ forwarder/src/main/java/com/example/forwarder/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.forwarder;
 
 import com.example.forwarder.db.DbService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,15 @@ public class RegistrationController {
     @Autowired
     private DbService dbService;
 
+    // Pass client's IP as identifier, then get clientUrl as parameter string and subscribedTopics as parameter string list
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.OK)
-    public void registerClient() {
+    public void registerClient(HttpServletRequest request) {
+        System.out.println(request.getRemoteAddr());
         dbService.saveClient(new com.example.forwarder.model.Client("client" + (int) (Math.random() * 10000), "http://localhost:8080", null));
     }
+    // 1) Figure out how to get user IP / HTTP headers, 2) Figure out how to pass parameters, 3) Make an "unregister" method?
+
+
+    // A "rest" way of registering and unregistering ???
 }
