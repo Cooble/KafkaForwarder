95acb963a6d281a2dffefe88ce7a7cd8a043f496
Jakub Švarc <jakubsvarc@protonmail.com>
2025-11-27 19:21:38 +0100

    prepare client

--- client/src/main/java/com/example/client/SimulationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.client;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SimulationService {
-}
--- client/src/main/java/com/example/client/WebSocketService.java
+++ client/src/main/java/com/example/client/WebSocketService.java
@@ -1,7 +1,116 @@
 package com.example.client;
 
+import com.example.common.ConfirmationRequest;
+import com.example.common.ExternalData;
+import com.example.common.RegistrationRequest;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class WebSocketService {
+    private static final Logger log = LoggerFactory.getLogger(WebSocketService.class);
+
+    @Value("${forwarder.url}")
+    private String forwarderUrl;
+
+    @Value("${client.url}")
+    private String clientUrl;
+
+    @Value("${client.subscribed.topics}")
+    private String subscribedTopicsStr;
+
+    @Value("${client.identifier:unknown}")
+    private String clientIdentifier;
+
+    private WebSocketStompClient stompClient;
+    private StompSession stompSession;
+
+    @PostConstruct
+    public void connect() {
+        List<Transport> transports = Arrays.asList(new WebSocketTransport(new StandardWebSocketClient()));
+        SockJsClient sockJsClient = new SockJsClient(transports);
+
+        stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        try {
+            stompSession = stompClient.connectAsync(forwarderUrl, new StompSessionHandlerAdapter() {
+                @Override
+                public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                    log.info("Connected to WebSocket at {}", forwarderUrl);
+
+                    final var topics = Arrays.asList(subscribedTopicsStr.split(","));
+                    
+                    for (final var topic : topics) {
+                        session.subscribe("/topic/" + topic, this);
+                        log.info("Subscribed to topic: {}", topic);
+                    }
+
+                    // Subscribe to user queue for retries
+                    session.subscribe("/user/queue/data", this);
+
+                    final var regRequest = new RegistrationRequest(clientUrl, topics);
+                    
+                    session.send("/app/register", regRequest);
+                    log.info("Sent registration for topics: {}", topics);
+                }
+
+                @Override
+                public void handleFrame(StompHeaders headers, Object payload) {
+                    if (payload instanceof ExternalData data) {
+                        log.info(
+                                "Received data via WS: id={}, msg={}, name={}, externalNew={}",
+                                data.id(),
+                                data.msg(),
+                                data.name(),
+                                data.externalNew()
+                        );
+
+                        log.info("Processing data: {}", data.name());
+                        // TODO add delay and eventual message dropping for simulation
+
+                        final var confirmRequest = new ConfirmationRequest(data.id(), clientIdentifier);
+                        
+                        stompSession.send("/app/confirm", confirmRequest);
+                        log.info("Sent confirmation for data ID {}", data.id());
+                    }
+                }
+
+                @Override
+                public Type getPayloadType(StompHeaders headers) {
+                    return ExternalData.class;
+                }
+
+                @Override
+                public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+                    log.error("WebSocket error: ", exception);
+                }
+
+                @Override
+                public void handleTransportError(StompSession session, Throwable exception) {
+                    log.error("WebSocket transport error: ", exception);
+                }
+            }).get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Failed to connect to WebSocket", e);
+        }
+    }
 }
